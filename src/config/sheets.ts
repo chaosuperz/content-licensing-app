06b317737eef8bc084d3f3760a5574ef6e98597f
Chao Zhou <chaozhou@mac.local.meter>
2025-09-02 15:12:58 -0700

    Update Google Script URL to redirect URL

--- src/config/sheets.ts
+++ src/config/sheets.ts
@@ -1,7 +1,7 @@
 // Google Sheets Configuration
 export const GOOGLE_SHEETS_CONFIG = {
   // Google Apps Script Web App URL
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzkf3AlNtKZ-7EWCYl5GZjoTbBqE8jPWDb97UCFL1yy9hr3j86ph4UzhtEeq4Y4rbVg/exec',
+  SCRIPT_URL: 'https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLh7axZauUKlJWsNiknOSlZ-H_porwOODiAbyImPx8hNOF0Swa-5ClaWWCyI3A8J0Zr_4nh9IYg7rdFK6I5mHchhG7ESUB2Sz2kn4oef6fuieEn0z9DeIJIyZ94J1ZOMjMWbnZMEwEZdjSnTABcZc0LPuJyMqnIv6u7-sjRGkJfWuoL6G4TrkLhBzxSozl0xCh72AltHSurMKo1Ai_iZfEhH0LWZ72AnreizQwyKrMWejUoo-zXKNFbJyGfhw84KaXn-PLByIUwQN8gtjb3H3JFpkZElvQ&lib=MERRq9i0_VmYjr6zcYIeMQmEtXh2Qkjou',
   
   // Sheet column mapping
   COLUMNS: {
